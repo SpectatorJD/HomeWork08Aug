39b3e7c336e7235b592223dda1f6396821d91c63
Tkakishak <tkakishakd@mail.ru>
2023-08-11 20:14:08 +0300

    StartCommit

---
+++ src/main/java/com/example/HomeWorkAug080823/HomeWorkAug080823Application.java
@@ -0,0 +1,13 @@
+package com.example.HomeWorkAug080823;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkAug080823Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkAug080823Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HomeWorkAug080823/HomeWorkAug080823ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeWorkAug080823;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkAug080823ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
